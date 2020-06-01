2d0435e65de6f0d072f47a5fff6b439857d0a393
Hunter-Ghost-27 <Shresht7@users.noreply.github.com>
2020-06-01 08:33:58 +0530

    Attempt at better organization of code.

--- Mods/S7_Config_de8f15f2-65a2-4ee4-a25f-7a7ab0305a58/Story/RawFiles/Lua/BootstrapServer.lua
+++ Mods/S7_Config_de8f15f2-65a2-4ee4-a25f-7a7ab0305a58/Story/RawFiles/Lua/BootstrapServer.lua
@@ -15,7 +15,7 @@ S7_DefaultSettings = {
     ["ManuallySynchronize"] = {}, --  statIDs listed here can be manually synchronized using diagnostics-option.
     ["ExportStatIDtoTSV"] = {
         ["FileName"] = "S7_Config_AllTheStats.tsv", --  FileName for ExportedStats.
-        ["RestrictStatTypeTo"] = {} --  Limit the search to only these statTypes. e.g. Character, Potions, SkillData.
+        ["RestrictStatTypeTo"] = {} --  limitedStats the search to only these statTypes. e.g. Character, Potions, SkillData.
     },
     ["BypassSafetyCheck"] = false --  Bypasses S7_SafeToModify() - Prevents modifications of unsupported or problematic keys.
 }
@@ -100,6 +100,9 @@ Ext.NewCall(S7_Config_ModMenuRelay, "S7_Config_ModMenuRelay", "(STRING)_Signal")
 --      STATS CONFIG AND SYNC
 --  #############################
 
+--  STATS-CONFIGURATOR
+--  ==================
+
 function S7_StatsConfigurator(JSONstring) --  Recieves stringified JSON from Ext.LoadFile(), Osiris or a mod.
     local JSONborne = Ext.JsonParse(JSONstring) --  Parsed JSONstring.
 
@@ -131,8 +134,38 @@ function S7_StatsConfigurator(JSONstring) --  Recieves stringified JSON from Ext
     S7_StatsSynchronize() --  Synchronize stats for all clients.
 end
 
+function S7_SafeToModify(key) --  Checks if key is safe to modify.
+    local dontFwith = {
+        --  Don't mess with these keys.
+        "MemorizationRequirements",
+        "Requirements",
+        "AoEConditions",
+        "TargetConditions",
+        "ForkingConditions",
+        "CycleConditions",
+        "SkillProperties",
+        "WinBoost",
+        "LoseBoost"
+    }
+
+    if S7_ConfigSettings.BypassSafetyCheck == true then --  Manual-Override setting is true.
+        return true --  S7_SafeToModify() returns true for everything.
+    else -- Default Setting
+        for i, avoid in ipairs(dontFwith) do --  Iterate over keys to avoid.
+            if key == avoid then --  If key matches.
+                return false --  Stop it right there.
+            else
+                return true --  else continue.
+            end
+        end
+    end
+end
+
+--  STATS-SYNCHRONIZE
+--  =================
+
 function S7_StatsSynchronize()
-    if type(next(toSync)) ~= "nil" then --  Stats were modified.
+    if type(next(toSync)) ~= "nil" then --  Stats were modified. toSync is not empty.
         Ext.Print(
             "[S7:Config - BootstrapServer.lua] --- Synchronizing Stats [Savegame-Persistence: " ..
                 tostring(S7_ConfigSettings.SyncStatPersistence) .. "]"
@@ -151,41 +184,43 @@ function S7_StatsSynchronize()
 end
 
 --  ################################
---      EXPORT STAT-NAMES TO TSV
+--        AUXILIARY FUNCTIONS
 --  ################################
 
-function S7_StatsExportTSV() --  Fetches literally every stat and exports to TSV
-    Ext.SaveFile(S7_ConfigSettings.ExportStatIDtoTSV.FileName, "") --  Creates an empty TSV or Overwrites the existing one.
-    local SaveAllStatsToFile = "S.No\tType\tStatID\n" --  Header Column
+--  EXPORT STATS TO TSV
+--  ===================
 
-    local allStat = {} --  Initialize temporary table
-    if type(next(S7_ConfigSettings.ExportStatIDtoTSV.RestrictStatTypeTo)) == "nil" then --  No restrictions in settings
+function S7_StatsExportTSV() --  Fetches literally every stat and exports to TSV.
+    Ext.SaveFile(S7_ConfigSettings.ExportStatIDtoTSV.FileName, "") --  Creates an empty TSV or Overwrites the existing one.
+    local SaveAllStatsToFile = "S.No\tType\tStatID\n" --  Header Column.
+
+    local allStat = {} --  Initialize temporary table.
+    if type(next(S7_ConfigSettings.ExportStatIDtoTSV.RestrictStatTypeTo)) == "nil" then --  No restrictions in settings.
         allStat = Ext.GetStatEntries() --  Get All Stat Entries
     else
-        for i, statType in ipairs(S7_ConfigSettings.ExportStatIDtoTSV.RestrictStatTypeTo) do --  Only selected statTypes are loaded
-            limit = Ext.GetStatEntries(statType)
-            for l, val in ipairs(limit) do
-                table.insert(allStat, val) --  appends selected stat-type entries to allStat
+        for i, statType in ipairs(S7_ConfigSettings.ExportStatIDtoTSV.RestrictStatTypeTo) do --  Only selected statTypes are loaded.
+            local limitedStats = Ext.GetStatEntries(statType)
+            for j, stats in ipairs(limitedStats) do
+                table.insert(allStat, stats) --  appends selected stat-type entries to allStat.
             end
         end
     end
 
     for key, value in ipairs(allStat) do
-        local type = NRD_StatGetType(value) --  Didn't I just filter stats by statType? - this is what happens when you return to old code with new ideas
-        SaveAllStatsToFile = SaveAllStatsToFile .. key .. "\t" .. type .. "\t" .. value .. "\n" --  Tab Separated Values
+        local type = NRD_StatGetType(value) --  Didn't I just filter stats by statType? - this is what happens when you return to old code with new ideas.
+        SaveAllStatsToFile = SaveAllStatsToFile .. key .. "\t" .. type .. "\t" .. value .. "\n" --  Tab Separated Values format.
     end
     Ext.SaveFile(S7_ConfigSettings.ExportStatIDtoTSV.FileName, SaveAllStatsToFile) --  Save TSV file.
 end
 
---  ###########################
---      INSPECT STATS SKILL
---  ###########################
+--  INSPECT SKILL
+--  =============
 
-local function S7_InspectStats(StatID, StatType) --  Recieves StatID and StatType from Osiris
+local function S7_InspectStats(StatID, StatType) --  Recieves StatID and StatType from Osiris.
     local compareStat = Ext.GetStatEntries(StatType) --  Retrieves all stat entries of corresponding stat-type for comparison.
-    for name, content in pairs(compareStat) do --  Iterate over compareStat
+    for name, content in pairs(compareStat) do --  Iterate over compareStat.
         if content == StatID then
-            Ext.Print("[S7:Config - BootstrapServer.lua] --- (" .. StatType .. "): " .. StatID)
+            Ext.Print("[S7:Config - BootstrapServer.lua] --- Inspected: (" .. StatType .. "): " .. StatID)
         end
     end
 end
@@ -193,37 +228,6 @@ end
 --  =================================================================================
 Ext.NewCall(S7_InspectStats, "S7_InspectStats", "(STRING)_StatID, (STRING)_StatType")
 --  =================================================================================
-
---  ###################################
---      SUPPORT FUNCTION DEFINITIONS
---  ###################################
-
-function S7_SafeToModify(key) --  Checks if key is safe to modify.
-    local dontFwith = {
-        --  Don't mess with these keys.
-        "MemorizationRequirements",
-        "Requirements",
-        "AoEConditions",
-        "TargetConditions",
-        "ForkingConditions",
-        "CycleConditions",
-        "SkillProperties",
-        "WinBoost",
-        "LoseBoost"
-    }
-
-    if S7_ConfigSettings.BypassSafetyCheck == true then --  Manual-Override setting is true
-        return true --  S7_SafeToModify() returns true for everything
-    else -- Default Setting
-        for i, avoid in ipairs(dontFwith) do --  Iterate over keys to avoid
-            if key == avoid then --  If key matches
-                return false --  Stop it right there
-            else
-                return true --  else continue
-            end
-        end
-    end
-end
 
 --  ####################
 --      ISSUE-TRACKER
