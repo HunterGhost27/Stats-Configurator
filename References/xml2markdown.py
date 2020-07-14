11050997cc260cd269d673fc894c2f6e304b4ab8
Hunter-Ghost-27 <Shresht7@users.noreply.github.com>
2020-07-14 10:39:54 +0530

    Added commentary to xml2markdown.py

--- References/xml2markdown.py
+++ References/xml2markdown.py
@@ -1,4 +1,6 @@
+#   =========================================================================================================================================
 #   =================================== Extract Data from XML files and create Markdown Reference Sheets.   =================================
+#   =========================================================================================================================================
 
 #   I hate XML!
 
@@ -7,7 +9,7 @@
 #   =========
 
 import pandas                   # For fancy dataframes.
-import pandas_read_xml as pdx   # To convert XML to dataframes.
+import pandas_read_xml as pdx   # To convert XML to fancy dataframes.
 
 #   =============
 #   READ XML FILE
@@ -16,74 +18,89 @@ import pandas_read_xml as pdx   # To convert XML to dataframes.
 enumerationsDataFrame = pdx.read_xml(
     "Ref_Enumerations.xml", ["root", "enumerations", "enumeration"])
 
-statObjectDefsDataFrame = pdx.read_xml("Ref_StatObjectDefinitions.xml", [
-    "root", "stat_object_definitions", "stat_object_definition"])
+statObjectDefsDataFrame = pdx.read_xml(
+    "Ref_StatObjectDefinitions.xml", ["root", "stat_object_definitions", "stat_object_definition"])
 
 #   ======================
 #   CREATE MARKDOWN TABLES
 #   ======================
 
 #   Enumerations.md
-#   ---------------
+#   ===============
+
+#       Markdown header
+#       ---------------
 
 enumerationsMarkdownContent = "# Reference: Enumerations\n\n---\n\n## Table of Contents\n\n"
 
 #       Table of Contents
 #       -----------------
 
+# e.g.:     - [Surface Type](#surface-type)
 for index, content in enumerationsDataFrame.iterrows():
-    enumerationsMarkdownContent += "- [" + content["@name"] + "]"
-    enumerationsMarkdownContent += "(#" + \
-        content["@name"].replace(" ", "-") + ")\n"
+    enumerationsMarkdownContent += "- [" + content["@name"] + \
+        "]" + "(#" + content["@name"].replace(" ", "-") + ")\n"
+
 enumerationsMarkdownContent += "\n---\n\n"
 
 #       Markdown Content
 #       ----------------
 
+#   Extract data as dataframe
 for index, content in enumerationsDataFrame.iterrows():
     try:
-        itemDataFrame = pandas.DataFrame.from_dict(
-            content["items"]["item"])
-    except ValueError:
+        itemDataFrame = pandas.DataFrame.from_dict(content["items"]["item"])
+    except ValueError:  # For Act and SkillElements that have only one entry.
         itemDataFrame = pandas.DataFrame([{"@index": 0, "@value": 1}])
 
-    enumerationsMarkdownContent += "## " + content["@name"] + "\n\n"
-    enumerationsMarkdownContent += pandas.DataFrame.to_markdown(
-        itemDataFrame) + "\n\n"
+#   Create markdown tables from dataframe
+    enumerationsMarkdownContent += "## " + content["@name"] + "\n\n" + \
+        pandas.DataFrame.to_markdown(itemDataFrame) + "\n\n"
 
 #   StatObjectDefinitions.md
-#   ------------------------
+#   ========================
+
+#       Markdown header
+#       ---------------
+
+statObjectDefsMarkdownContent = "# Reference: Stat Object Definitions\n\n---\n\n## Table of Contents\n\n"
 
 #       Table of Contents
 #       -----------------
 
-statObjectDefsMarkdownContent = "# Reference: Stat Object Definitions\n\n---\n\n## Table of Contents\n\n"
-
+#   Reduced data-frame
 statObjectDefsDataFrame = statObjectDefsDataFrame[[
     "@name", "@category", "field_definitions"]]
+
+#   e.g.:   - [ItemCombo: ItemComboProperties](#itemcombo-itemcomboproperties)
 for index, content in statObjectDefsDataFrame.iterrows():
     text = content["@category"] + ": " + content["@name"]
-    statObjectDefsMarkdownContent += "- [" + text + \
-        "](#" + text.replace(" ", "-") + ")\n"
+    statObjectDefsMarkdownContent += "- [" + \
+        text + "](#" + text.replace(" ", "-") + ")\n"
+
 statObjectDefsMarkdownContent += "\n---\n\n"
 
 #       Markdown Content
 #       ----------------
 
 for index, content in statObjectDefsDataFrame.iterrows():
+    #   Markdown table-title
     statObjectDefsMarkdownContent += "## " + \
         content["@category"] + ": " + content["@name"] + "\n\n"
+    #   Extract data as data-frame
     fieldDefsDataFrame = pandas.DataFrame.from_dict(
         content["field_definitions"]["field_definition"])
+    #   Clean dataframe
     fieldDefsDataFrame = fieldDefsDataFrame.drop(
         columns=["@display_name", "@export_name"])
     fieldDefsDataFrame = fieldDefsDataFrame.dropna(how="all")
     fieldDefsDataFrame = fieldDefsDataFrame.fillna("")
+    #   Move description column to the end
     cols = list(fieldDefsDataFrame.columns.values)
     if "@description" in cols:
         cols.pop(cols.index("@description"))
         fieldDefsDataFrame = fieldDefsDataFrame[cols + ["@description"]]
-
+    #   Create markdown table
     statObjectDefsMarkdownContent += pandas.DataFrame.to_markdown(
         fieldDefsDataFrame) + "\n\n"
 
@@ -91,13 +108,16 @@ for index, content in statObjectDefsDataFrame.iterrows():
 #   WRITE MARKDOWN FILE
 #   ===================
 
+#   Extra pedantic removal of the last "\n"
 enumerationsMarkdownContent = enumerationsMarkdownContent.rstrip() + "\n"
 statObjectDefsMarkdownContent = statObjectDefsMarkdownContent.rstrip() + "\n"
 
+#   Write Enumerations.md
 with open("Enumerations.md", "w") as markdownFileEnumerations:
     markdownFileEnumerations.write(enumerationsMarkdownContent)
 markdownFileEnumerations.close()
 
+#   Write StatObjectDefinitions.md
 with open("StatObjectDefinitions.md", "w") as markdownFilestatObjectDefs:
     markdownFilestatObjectDefs.write(statObjectDefsMarkdownContent)
 markdownFilestatObjectDefs.close()
