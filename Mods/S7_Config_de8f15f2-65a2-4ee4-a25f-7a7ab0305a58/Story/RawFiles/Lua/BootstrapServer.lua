cda8eba1854597232ad7c157cba44d00c0fb6c5e
Hunter-Ghost-27 <Shresht7@users.noreply.github.com>
2021-01-15 00:34:08 +0530

    Moved DialogMenus out of folder

--- Mods/S7_Config_de8f15f2-65a2-4ee4-a25f-7a7ab0305a58/Story/RawFiles/Lua/BootstrapServer.lua
+++ Mods/S7_Config_de8f15f2-65a2-4ee4-a25f-7a7ab0305a58/Story/RawFiles/Lua/BootstrapServer.lua
@@ -3,8 +3,8 @@
 --  =======
 
 Ext.Require("Auxiliary.lua")
-Ext.Require("Server/DialogMenus/ModMenu.lua")
-Ext.Require("Server/DialogMenus/ModInterface.lua")
+Ext.Require("Server/ModMenu.lua")
+Ext.Require("Server/ModInterface.lua")
 Ext.Require("Server/InspectSkill.lua")
 Ext.Require("Server/Networking.lua")
 Ext.Require("Shared/Collections.lua")
