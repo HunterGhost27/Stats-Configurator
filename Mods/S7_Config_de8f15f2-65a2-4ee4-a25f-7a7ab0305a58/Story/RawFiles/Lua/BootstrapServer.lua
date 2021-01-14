1f181b43000ed4898ad76d4772000b79b5539373
Hunter-Ghost-27 <Shresht7@users.noreply.github.com>
2021-01-15 00:31:10 +0530

    Revived Inspect

--- Mods/S7_Config_de8f15f2-65a2-4ee4-a25f-7a7ab0305a58/Story/RawFiles/Lua/BootstrapServer.lua
+++ Mods/S7_Config_de8f15f2-65a2-4ee4-a25f-7a7ab0305a58/Story/RawFiles/Lua/BootstrapServer.lua
@@ -5,6 +5,7 @@
 Ext.Require("Auxiliary.lua")
 Ext.Require("Server/DialogMenus/ModMenu.lua")
 Ext.Require("Server/DialogMenus/ModInterface.lua")
+Ext.Require("Server/InspectSkill.lua")
 Ext.Require("Server/Networking.lua")
 Ext.Require("Shared/Collections.lua")
 Ext.Require("Shared/References.lua")
--- Mods/S7_Config_de8f15f2-65a2-4ee4-a25f-7a7ab0305a58/Story/RawFiles/Lua/Server/InspectSkill.lua
+++ Mods/S7_Config_de8f15f2-65a2-4ee4-a25f-7a7ab0305a58/Story/RawFiles/Lua/Server/InspectSkill.lua
@@ -18,11 +18,31 @@ end)
 --  INSPECT SKILL
 --  =============
 
-Ext.RegisterOsirisListener("CharacterUsedSkillOnTarget", 5, "after", function (character, target, skill, ...)
-    if character ~= Osi.CharacterGetHostCharacter() and skill ~= "Target_S7_Config_Inspect" then return end
+Ext.RegisterOsirisListener("CharacterUsedSkillOnTarget", 5, "after", function (char, tar, skill, ...)
+    if char ~= Osi.CharacterGetHostCharacter() and skill ~= "Target_S7_Config_Inspect" then return end
 
-    --  Target is character. Inspect target skills.
-    --  Target is character. Inspect target statuses.
-    --  Target is character. Inspect characterStatName.
-    --  Target is item. Inspect ItemStatID.
+    local characterGUID = ExtractGUID(char)
+    local character = Ext.GetCharacter(characterGUID)
+    local targetGUID = ExtractGUID(tar)
+    local target = Osi.ObjectIsCharacter(targetGUID) == 1 and Ext.GetCharacter(targetGUID) or Ext.GetItem(targetGUID)
+
+    Stringer:SetHeader('Inspecting: ' .. tostring(tar))
+
+    if Osi.ObjectIsCharacter(targetGUID) == 1 then
+    local skills = target:GetSkills()
+        Stringer:Add('Skills:')
+        Stringer:LineBreak('-')
+        for _, skill in pairs(skills) do Stringer:Add(skill) end
+        Stringer:LineBreak('_')
+    end
+
+    local statuses = target:GetStatuses()
+    if IsValid(statuses) then
+        Stringer:Add('Statuses: ')
+        Stringer:LineBreak('-')
+        for _, status in pairs(statuses) do Stringer:Add(status) end
+        Stringer:LineBreak('_')
+    end
+
+    Debug:FPrint(Stringer:Build())
 end)
