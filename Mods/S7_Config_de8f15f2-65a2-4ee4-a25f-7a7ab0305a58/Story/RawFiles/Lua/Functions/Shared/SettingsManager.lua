c32a85bdc706294eaa68a5cafcefdbc18959593c
Hunter-Ghost-27 <Shresht7@users.noreply.github.com>
2021-01-14 19:03:22 +0530

    Update SettingsManager

--- Mods/S7_Config_de8f15f2-65a2-4ee4-a25f-7a7ab0305a58/Story/RawFiles/Lua/Functions/Shared/SettingsManager.lua
+++ Mods/S7_Config_de8f15f2-65a2-4ee4-a25f-7a7ab0305a58/Story/RawFiles/Lua/Functions/Shared/SettingsManager.lua
@@ -3,7 +3,39 @@
 --  ================
 
 ---@class Settings @Mod-Settings
+Settings = {}
+DefaultSettings = DefaultSettings or {}
+
 Settings = Integrate(DefaultSettings, MODINFO.ModSettings)
+
+---Loads CENTRAL file and Updates settings
+function Settings:Load()
+    CENTRAL = CENTRAL:Load()
+    self:Update(CENTRAL[IDENTIFIER].ModSettings)
+end
+
+---Updates settings on a case-by-case basis
+---@param parent Settings
+---@param settings Settings
+---@param default Settings
+local function updateSetting(parent, settings, default)
+    for key, value in pairs(default) do
+        if type(value) == 'table' then updateSetting(parent[key], settings[key], value) end
+        if parent[key] == settings[key] then break end
+        Stringer:Add(key .. ": " .. tostring{parent[key] .. " -> " .. tostring(settings[key])})
+        parent[key] = settings[key] or value
+    end
+end
+
+---Updates Settings
+---@param settings Settings
+function Settings:Update(settings)
+    local settings = settings or {}
+    Stringer:SetHeader('Updating Mod-Settings')
+    updateSetting(self, settings, DefaultSettings)
+    Debug:Print(Stringer:Build())
+    self:Sync()
+end
 
 ---Synchronizes Settings with MODINFO.ModSettings and PersistentVars.Settings
 function Settings:Sync()
@@ -11,18 +43,11 @@ function Settings:Sync()
     PersistentVars.Settings = Rematerialize(self)
 end
 
----Loads CENTRAL file and Updates settings
-function Settings:Load()
-    CENTRAL = LoadFile('S7Central.json') or {}
-    self:Update(CENTRAL[IDENTIFIER].ModSettings)
-end
-
----Updates Settings
----@param settings Settings
-function Settings:Update(settings)
-    local settings = settings or {}
-    self = Integrate(self, settings)
-    self:Sync()
+---Saves Settings in CENTRAL file
+function Settings:Save()
+   self:Sync()
+   CENTRAL:Sync()
+   CENTRAL:Save()
 end
 
 --  ============================================================================================
