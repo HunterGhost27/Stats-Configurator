8b118384d4124db9d881f9bc9248852723b1352a
Hunter-Ghost-27 <Shresht7@users.noreply.github.com>
2021-01-14 19:15:44 +0530

    Update FilesManager

--- Mods/S7_Config_de8f15f2-65a2-4ee4-a25f-7a7ab0305a58/Story/RawFiles/Lua/Functions/Shared/Files.lua
+++ Mods/S7_Config_de8f15f2-65a2-4ee4-a25f-7a7ab0305a58/Story/RawFiles/Lua/Functions/Shared/Files.lua
@@ -11,7 +11,7 @@ function LoadFile(fileName, context)
     local file
     local _, fileContents = pcall(Ext.LoadFile, fileName, context)
     if string.match(fileName, '.json') then
-        if ValidString(fileContents) then file = Ext.JsonParse(fileContents)
+        if ValidString(fileContents) then file = Ext.JsonParse(fileContents); Destringify(file)
         else file = {} end
     else file = fileContents end
     return file, fileContents
@@ -29,4 +29,25 @@ function SaveFile(fileName, contents)
         local fileContents = type(contents) == 'table' and Ext.JsonStringify(Rematerialize(contents)) or tostring(contents) or ""
         Ext.SaveFile(fileName, fileContents)
     end
+end
+
+--  ===================
+--  LOAD MULTIPLE FILES
+--  ===================
+
+---Loads multiple files
+---@param fileNames table Array of fileNames
+---@param context string Path-Context
+---@return table files Table of files
+function LoadFiles(fileNames, context)
+    local files
+    if type(fileNames) ~= 'table' then return end
+    for _, fileName in ipairs(fileNames) do
+        local _, fileContents = pcall(Ext.LoadFile, fileName, context)
+        if string.match(fileName, '.json') then
+            if ValidString(fileContents) then files[fileName] = Ext.JsonParse(fileContents); Destringify(files[fileName])
+            else files[fileName] = {} end
+        else files[fileName] = fileContents end
+    end
+    return files
 end
