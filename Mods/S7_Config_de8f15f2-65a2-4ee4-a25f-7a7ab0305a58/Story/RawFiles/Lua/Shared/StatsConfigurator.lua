d0334d903a80b7e404e87cfe0dcf9b572d45dde2
Hunter-Ghost-27 <Shresht7@users.noreply.github.com>
2021-01-22 09:20:55 +0530

    Refactor StatsConfigurator

--- Mods/S7_Config_de8f15f2-65a2-4ee4-a25f-7a7ab0305a58/Story/RawFiles/Lua/Shared/StatsConfigurator.lua
+++ Mods/S7_Config_de8f15f2-65a2-4ee4-a25f-7a7ab0305a58/Story/RawFiles/Lua/Shared/StatsConfigurator.lua
@@ -9,52 +9,31 @@ Stats = {
 
 function Stats:Configurator()
     Stringer:SetHeader(Settings.StatsLoader.FileName .. " loaded. Applying configuration profile.")
-    for statName, config in pairs(self.Configurations) do
+    ForEach(self.Configurations, function(key, config)
         if type(config) ~= 'table' then return end
-        local statList = Collections:Unpack(statName)
-        for name, bool in pairs(statList) do
-            local stat = Ext.GetStat(name)
-            if not stat then return end
-            Stringer:Add(name)
-            Stringer:LineBreak('-')
 
-            for key, value in pairs(config) do
-                if not SafeToModify(name, key) then else
-                Stringer:Add(tostring(key) .. ": " .. tostring(value) .. " (" .. tostring(stat[key]) .. ")")
-                stat[key] = value
-                end
-            end
+        local statList = Collections:Unpack(key)
+        ForEach(statList, function(statName, bool)
+            if not bool then return end
+
+            local stat = Ext.GetStat(statName)
+            if not stat then return end
+
+            Stringer:Add(statName)
+            Stringer:LineBreak('-')
+            ForEach(config, function(attribute, value)
+                if not SafeToModify(statName, attribute) then return end
+                Stringer:Add(tostring(attribute) .. ": " .. tostring(value) .. " (" .. tostring(stat[attribute]) .. ")")
+                stat[attribute] = value
+            end)
             Stringer:LineBreak('_')
-            self.Synchronizations[name] = true
-        end
-        self.Configurations[statName] = nil
-    end
+            self.Synchronizations[statName] = true
+        end)
+        self.Configurations[key] = nil
+    
+    end)
     Debug:Print(Stringer:Build())
     Debug:FPrint('Configuration Profile Active')
-end
-
-function SafeToModify(name, key)
-    if Settings.BypassSafetyCheck then return true end
-
-    local dontFwith = {
-        ["AoEConditions"] = true,
-        ["TargetConditions"] = true,
-        ["ForkingConditions"] = true,
-        ["CycleConditions"] = true,
-        ["SkillProperties"] = true,
-        ["WinBoost"] = true,
-        ["LoseBoost"] = true,
-        ["RootTemplate"] = true
-    } --  dont mess with these keys
-
-
-    if dontFwith[key] then
-        Debug:Warn("SafeToModify() prevents modification of " .. key .. " [BypassSafetyCheck: " .. tostring(Settings.BypassSafetyCheck) .. "]")
-        return false
-    else
-        if Ext.StatGetAttribute(name, key) then return true
-        else Debug:Warn(key .. " is not a valid attribute for " .. name) end
-    end
 end
 
 --  STATS-SYNCHRONIZE
@@ -78,4 +57,32 @@ function BuildConfigData(buildData)
     local configData = LoadFile(MODINFO.SubdirPrefix .. Settings.StatsLoader.FileName) or {}
     for key, value in pairs(buildData) do configData[key] = value end
     SaveFile(MODINFO.SubdirPrefix .. Settings.StatsLoader.FileName, configData)
+end
+
+--  ==============
+--  SAFE TO MODIFY
+--  ==============
+
+function SafeToModify(statName, attribute)
+    if Settings.BypassSafetyCheck then return true end
+
+    local dontFwith = {
+        ["AoEConditions"] = true,
+        ["TargetConditions"] = true,
+        ["ForkingConditions"] = true,
+        ["CycleConditions"] = true,
+        ["SkillProperties"] = true,
+        ["WinBoost"] = true,
+        ["LoseBoost"] = true,
+        ["RootTemplate"] = true
+    } --  dont mess with these keys
+
+
+    if dontFwith[attribute] then
+        Debug:Warn("SafeToModify() prevents modification of " .. attribute .. " [BypassSafetyCheck: " .. tostring(Settings.BypassSafetyCheck) .. "]")
+        return false
+    else
+        if Ext.StatGetAttribute(statName, attribute) then return true
+        else Debug:Warn(attribute .. " is not a valid attribute for " .. statName) return false end
+    end
 end
