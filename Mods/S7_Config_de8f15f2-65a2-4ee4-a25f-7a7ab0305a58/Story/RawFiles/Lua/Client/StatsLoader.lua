5d03d18893418b0bf437659521120c7422e9db85
Hunter-Ghost-27 <Shresht7@users.noreply.github.com>
2021-01-31 05:38:02 +0530

    Refactor StatsLoader

--- Mods/S7_Config_de8f15f2-65a2-4ee4-a25f-7a7ab0305a58/Story/RawFiles/Lua/Client/StatsLoader.lua
+++ Mods/S7_Config_de8f15f2-65a2-4ee4-a25f-7a7ab0305a58/Story/RawFiles/Lua/Client/StatsLoader.lua
@@ -14,13 +14,9 @@ function StatsLoader()
     local configData = LoadFile(MODINFO.SubdirPrefix .. Settings.StatsLoader.FileName) or {}
 
     ForEach(configData, function (key, value)
-        if key == 'Cache' then return end
+        if key == 'Cache' then Stats.Memoizer.Cache = Integrate(Stats.Memoizer, value) return end
         Stats.Configurations[key] = value
     end)
-
-    Stats.Memoizer:LoadCache(MODINFO.SubdirPrefix .. Settings.StatsLoader.FileName)
-
-    Ext.Print(Ext.JsonStringify(Rematerialize(Stats)))
 
     --  CALL CONFIGURATOR
     --  =================
