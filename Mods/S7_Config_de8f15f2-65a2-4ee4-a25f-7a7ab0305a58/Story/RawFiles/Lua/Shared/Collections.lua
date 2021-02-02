b21b4c5c8994eafd6ef2181c90ca8b103fc02530
Hunter-Ghost-27 <Shresht7@users.noreply.github.com>
2021-02-02 19:21:01 +0530

    Improve Collections

--- Mods/S7_Config_de8f15f2-65a2-4ee4-a25f-7a7ab0305a58/Story/RawFiles/Lua/Shared/Collections.lua
+++ Mods/S7_Config_de8f15f2-65a2-4ee4-a25f-7a7ab0305a58/Story/RawFiles/Lua/Shared/Collections.lua
@@ -7,113 +7,113 @@ Collections = {
     --  CHARACTERS
     --  ----------
 
-    ["Character"] = {},
-    ["PlayerCharacters"] = {
+    ['Character'] = {},
+    ['PlayerCharacters'] = {
         --  Thanks to LaughingLeader. table taken from https://gist.github.com/LaughingLeader/3d4d23540852679260b988936799c1fe
-        ["_Hero"] = true,
-        ["HumanFemaleHero"] = true,
-        ["HumanMaleHero"] = true,
-        ["DwarfFemaleHero"] = true,
-        ["DwarfMaleHero"] = true,
-        ["ElfFemaleHero"] = true,
-        ["ElfMaleHero"] = true,
-        ["LizardFemaleHero"] = true,
-        ["LizardMaleHero"] = true,
-        ["HumanUndeadFemaleHero"] = true,
-        ["HumanUndeadMaleHero"] = true,
-        ["DwarfUndeadFemaleHero"] = true,
-        ["DwarfUndeadMaleHero"] = true,
-        ["ElfUndeadFemaleHero"] = true,
-        ["ElfUndeadMaleHero"] = true,
-        ["LizardUndeadFemaleHero"] = true,
-        ["LizardUndeadMaleHero"] = true,
-        ["_Companions"] = true,
-        ["StoryPlayer"] = true,
-        ["CasualPlayer"] = true,
-        ["NormalPlayer"] = true,
-        ["HardcorePlayer"] = true,
-        ["Player_Ifan"] = true,
-        ["Player_Lohse"] = true,
-        ["Player_RedPrince"] = true,
-        ["Player_Sebille"] = true,
-        ["Player_Beast"] = true,
-        ["Player_Fane"] = true
+        ['_Hero'] = true,
+        ['HumanFemaleHero'] = true,
+        ['HumanMaleHero'] = true,
+        ['DwarfFemaleHero'] = true,
+        ['DwarfMaleHero'] = true,
+        ['ElfFemaleHero'] = true,
+        ['ElfMaleHero'] = true,
+        ['LizardFemaleHero'] = true,
+        ['LizardMaleHero'] = true,
+        ['HumanUndeadFemaleHero'] = true,
+        ['HumanUndeadMaleHero'] = true,
+        ['DwarfUndeadFemaleHero'] = true,
+        ['DwarfUndeadMaleHero'] = true,
+        ['ElfUndeadFemaleHero'] = true,
+        ['ElfUndeadMaleHero'] = true,
+        ['LizardUndeadFemaleHero'] = true,
+        ['LizardUndeadMaleHero'] = true,
+        ['_Companions'] = true,
+        ['StoryPlayer'] = true,
+        ['CasualPlayer'] = true,
+        ['NormalPlayer'] = true,
+        ['HardcorePlayer'] = true,
+        ['Player_Ifan'] = true,
+        ['Player_Lohse'] = true,
+        ['Player_RedPrince'] = true,
+        ['Player_Sebille'] = true,
+        ['Player_Beast'] = true,
+        ['Player_Fane'] = true
     },
-    ["NonPlayerCharacters"] = {},
+    ['NonPlayerCharacters'] = {},
 
     --  ARMOR
     --  -----
 
-    ["Armor"] = {},
-    ["ArmorTypeCloth"] = {},
-    ["ArmorTypeLeather"] = {},
-    ["ArmorTypeMail"] = {},
-    ["ArmorTypePlate"] = {},
-    ["ArmorTypeRobe"] = {},
-    ["ArmorSlotHelmet"] = {},
-    ["ArmorSlotBreast"] = {},
-    ["ArmorSlotLeggings"] = {},
-    ["ArmorSlotWeapon"] = {},
-    ["ArmorSlotShield"] = {},
-    ["ArmorSlotRing"] = {},
-    ["ArmorSlotBelt"] = {},
-    ["ArmorSlotBoots"] = {},
-    ["ArmorSlotGloves"] = {},
-    ["ArmorSlotAmulet"] = {},
-    ["ArmorSlotRing2"] = {},
-    ["ArmorSlotWings"] = {},
-    ["ArmorSlotHorns"] = {},
-    ["ArmorSlotOverhead"] = {},
+    ['Armor'] = {},
+    ['ArmorTypeCloth'] = {},
+    ['ArmorTypeLeather'] = {},
+    ['ArmorTypeMail'] = {},
+    ['ArmorTypePlate'] = {},
+    ['ArmorTypeRobe'] = {},
+    ['ArmorSlotHelmet'] = {},
+    ['ArmorSlotBreast'] = {},
+    ['ArmorSlotLeggings'] = {},
+    ['ArmorSlotWeapon'] = {},
+    ['ArmorSlotShield'] = {},
+    ['ArmorSlotRing'] = {},
+    ['ArmorSlotBelt'] = {},
+    ['ArmorSlotBoots'] = {},
+    ['ArmorSlotGloves'] = {},
+    ['ArmorSlotAmulet'] = {},
+    ['ArmorSlotRing2'] = {},
+    ['ArmorSlotWings'] = {},
+    ['ArmorSlotHorns'] = {},
+    ['ArmorSlotOverhead'] = {},
 
     --  CRIME
     --  -----
 
-    ["Crime"] = {},
+    ['Crime'] = {},
 
     --  OBJECT
     --  ------
 
-    ["Object"] = {},
+    ['Object'] = {},
 
     --  POTION
     --  ------
 
-    ["Potion"] = {},
-    ["IsConsumable"] = {},
-    ["IsFood"] = {},
-    ["IsPotion"] = {},
+    ['Potion'] = {},
+    ['IsConsumable'] = {},
+    ['IsFood'] = {},
+    ['IsPotion'] = {},
 
     --  SHIELD
     --  ------
 
-    ["Shield"] = {},
+    ['Shield'] = {},
 
     --  SKILLDATA
     --  ---------
 
-    ["SkillData"] = {},
+    ['SkillData'] = {},
 
     --  STATUSDATA
     --  ----------
 
-    ["StatusData"] = {},
+    ['StatusData'] = {},
 
     --  WEAPON
     --  ------
 
-    ["Weapon"] = {},
-    ["WeaponTypeSword"] = {},
-    ["WeaponTypeClub"] = {},
-    ["WeaponTypeAxe"] = {},
-    ["WeaponTypeStaff"] = {},
-    ["WeaponTypeBow"] = {},
-    ["WeaponTypeCrossbow"] = {},
-    ["WeaponTypeSpear"] = {},
-    ["WeaponTypeKnife"] = {},
-    ["WeaponTypeWand"] = {},
-    ["WeaponTypeArrow"] = {},
-    ["WeaponIsTwoHanded"] = {},
-    ["WeaponIsNotTwoHanded"] = {}
+    ['Weapon'] = {},
+    ['WeaponTypeSword'] = {},
+    ['WeaponTypeClub'] = {},
+    ['WeaponTypeAxe'] = {},
+    ['WeaponTypeStaff'] = {},
+    ['WeaponTypeBow'] = {},
+    ['WeaponTypeCrossbow'] = {},
+    ['WeaponTypeSpear'] = {},
+    ['WeaponTypeKnife'] = {},
+    ['WeaponTypeWand'] = {},
+    ['WeaponTypeArrow'] = {},
+    ['WeaponIsTwoHanded'] = {},
+    ['WeaponIsNotTwoHanded'] = {}
 }
 
 --  ===================
@@ -124,8 +124,8 @@ Collections = {
 --  ==================
 
 function Collections:CustomRebuild()
-    local customCollections = LoadFile(MODINFO.SubdirPrefix .. "ConfigCollections.json") or {}
-    Collections = Integrate(Collections, customCollections)
+    local customCollections = LoadFile(MODINFO.SubdirPrefix .. 'ConfigCollections.json') or {}
+    self = Integrate(self, customCollections)
 end
 
 --  PREDEF-COLLECTIONS
@@ -134,15 +134,15 @@ end
 function Collections:Rebuild()
 
     local statTypeTable = {
-        "Armor",
-        "Character",
-        "Crime",
-        "Object",
-        "Potion",
-        "Shield",
-        "SkillData",
-        "StatusData",
-        "Weapon"
+        'Armor',
+        'Character',
+        'Crime',
+        'Object',
+        'Potion',
+        'Shield',
+        'SkillData',
+        'StatusData',
+        'Weapon'
     }
 
     for _, statType in pairs(statTypeTable) do
@@ -155,7 +155,7 @@ function Collections:Rebuild()
             --  GENERAL CATEGORIZATION
             --  ----------------------
 
-            Collections[statType][stat] = true
+            self[statType][stat] = true
 
             --  --------------------------
             --  SPECIALIZED CATEGORIZATION
@@ -164,57 +164,57 @@ function Collections:Rebuild()
             --  CHARACTERS
             --  ----------
 
-            if statType == "Character" then
+            if statType == 'Character' then
 
                 --  NON PLAYER CHARACTERS
 
-                if not Collections["PlayerCharacters"][stat] then Collections["NonPlayerCharacters"][stat] = true end
+                if not self['PlayerCharacters'][stat] then self['NonPlayerCharacters'][stat] = true end
 
             --  ARMOR
             --  -----
 
-            elseif statType == "Armor" then
+            elseif statType == 'Armor' then
 
                 --  ARMOR TYPE
 
-                if statData["ArmorType"] ~= "None" and ValidString(statData["ArmorType"]) then
-                    Collections["ArmorType" .. statData["ArmorType"]][stat] = true
+                if statData['ArmorType'] ~= 'None' and ValidString(statData['ArmorType']) then
+                    self['ArmorType' .. statData['ArmorType']][stat] = true
                 end
 
                 --  ARMOR SLOTS
 
-                if ValidString(statData["Slot"]) then
-                    Collections["ArmorSlot" .. statData["Slot"]][stat] = true
+                if ValidString(statData['Slot']) then
+                    self['ArmorSlot' .. statData['Slot']][stat] = true
                 end
 
             --  POTION
             --  ------
 
-            elseif statType == "Potion" then
+            elseif statType == 'Potion' then
 
                 --  IS CONSUMABLE
 
-                if statData["IsConsumable"] == "Yes" then
-                    Collections["IsConsumable"][stat] = true
-                    if statData["IsFood"] == "Yes" then Collections["IsFood"][stat] = true
-                    else Collections["IsPotion"][stat] = true end
+                if statData['IsConsumable'] == 'Yes' then
+                    self['IsConsumable'][stat] = true
+                    if statData['IsFood'] == "Yes" then self['IsFood'][stat] = true
+                    else self['IsPotion'][stat] = true end
                 end
 
             --  WEAPONS
             --  -------
 
-            elseif statType == "Weapon" then
+            elseif statType == 'Weapon' then
 
                 --  WEAPON-TYPE
 
-                if statData["WeaponType"] ~= "None" and ValidString(statData["WeaponType"]) then
-                    Collections["WeaponType" .. statData["WeaponType"]][stat] = true
+                if statData['WeaponType'] ~= 'None' and ValidString(statData['WeaponType']) then
+                    self['WeaponType' .. statData['WeaponType']][stat] = true
                 end
 
                 --  IS-TWO-HANDED
 
-                if statData["IsTwoHanded"] == "Yes" then Collections["WeaponIsTwoHanded"][stat] = true
-                else Collections["WeaponIsNotTwoHanded"][stat] = true end
+                if statData['IsTwoHanded'] == "Yes" then self['WeaponIsTwoHanded'][stat] = true
+                else self['WeaponIsNotTwoHanded'][stat] = true end
             end
         end
     end
@@ -222,14 +222,14 @@ function Collections:Rebuild()
     --  CUSTOM COLLECTIONS
     --  ------------------
 
-    Collections:CustomRebuild()
+    self:CustomRebuild()
 
     Debug:FPrint('Collections Rebuilt')
 end
 
 function Collections:Unpack(keyName)
     local returnNameList = {}
-    local collectionName = string.match(keyName, "^#(.-)$")
+    local collectionName = string.match(keyName, '^#(.-)$')
     if not collectionName then returnNameList[keyName] = true; return returnNameList end
     if self[collectionName] then
         Debug:Print("Unpacking collection " .. collectionName)
@@ -243,7 +243,7 @@ end
 
 ConsoleCommander:Register({
     ['Name'] = 'RebuildCollections',
-    ['Description'] = 'Rebuilds Collection Data',
+    ['Description'] = 'Rebuild Collection Data',
     ['Context'] = 'Shared',
     ['Action'] = Collections.Rebuild
 })
